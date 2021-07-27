18ffc9f3b5b35a3a030d8da79fcf180689ceabb3
amoschou <4948673+amoschou@users.noreply.github.com>
2021-07-27 21:06:27 +0930

    Added config file

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    lang: 'en-AU',
+    title: 'VuePress A',
+    description: 'This is a VuePress site.',
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+    },
+}
+
